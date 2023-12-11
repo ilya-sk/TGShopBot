0219f3b5c588dfbf85a4bb2997fbf979bdd5f673
Илья С <ilas@192.168.1.8>
2023-12-11 18:44:30 +0200

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 const TelegramBot = require("node-telegram-bot-api");
+const express = require('express');
+const cors = require('cors');
+
 
 const token = "6795720145:AAHVDpDZNU04xLgBis0g_LX-QXFLsvjoQmk";
-const webAppUrl = "https://ya.ru";
+const webAppUrl = "https://resilient-alfajores-9cbe8b.netlify.app";
 
 const bot = new TelegramBot(token, { polling: true });
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
 
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
   if (text === "/start") {
-    await bot.sendMessage(chatId, "Received your message", {
+    await bot.sendMessage(chatId, "В данный момент есть", {
       reply_markup : {
         inline_keyboard : [
-          [{text: 'Перейти в магазин', web_app: {url: webAppUrl}}]
+          [{text: 'Купить', web_app: {url: webAppUrl}}]
         ]
       }
     });
   }
 });
+
+app.post('/web-data', async (req, res) => {
+  const {queryId, products = [], totalPrice} = req.body;
+  try {
+      await bot.answerWebAppQuery(queryId, {
+          type: 'article',
+          id: queryId,
+          title: 'Успешная покупка',
+          input_message_content: {
+              message_text: ` Поздравляю с покупкой, вы приобрели товар на сумму ${totalPrice}, ${products.map(item => item.title).join(', ')}`
+          }
+      })
+      return res.status(200).json({});
+  } catch (e) {
+      return res.status(500).json({})
+  }
+})
+
+const PORT = 8000;
+
+app.listen(PORT, () => console.log('server started on PORT ' + PORT))
