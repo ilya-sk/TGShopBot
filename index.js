242a3f4198513143363eee89bfaa5ffda3fae832
Илья С <ilas@192.168.1.8>
2023-12-13 12:26:15 +0200

    новая кнопка

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ bot.on('message', async (msg) => {
 
         await bot.sendMessage(chatId, 'Заходи в наш интернет магазин по кнопке ниже', {
             reply_markup: {
-                inline_keyboard: [
+                keyboard: [
                     [{text: 'Сделать заказ', web_app: {url: webAppUrl}}]
                 ]
             }
